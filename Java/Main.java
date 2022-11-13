0cbc793037705c87cf1daca53f143c548f919a04
Victor Osorio <vimosorioga@unal.edu.co>
2022-11-12 18:48:35 -0800

    Creo objetos de prueba para python y java, además de un método en java para imprimir la info de driver y license

--- Java/Main.java
+++ Java/Main.java
@@ -2,5 +2,18 @@ class Main{
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andrés Herrera";
+        car.passenger = 4;
+        car.printDataCar();
+
+
+        Car car2 = new Car();
+        car2.license = "JAK067";
+        car2.driver = "Paola Agudelo";
+        car2.passenger = 3;
+        car2.printDataCar();
+        
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo!")
+    car = Car()
+    car.license = "JPU879"
+    car.driver = "Juan Pablo Zapata"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "HJU789"
+    car2.driver = "Martha Fernández"
+    print(vars(car2))

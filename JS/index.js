d7d30c3e65c8fdbec043dc066a82dd38cfc47f05
Victor Osorio <vimosorioga@unal.edu.co>
2022-11-12 21:55:07 -0800

    Creo los métodos constructores para java y javascript

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AWS547", new Account("Federico Padilla","FED543"));
+car.passenger = 4;
+car.printDataCar();
--- Java/Main.java
+++ Java/Main.java
@@ -2,16 +2,12 @@ class Main{
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andrés Herrera";
+        Car car = new Car("AMQ123", new Account("Andrés Herrera", "AND123"));
         car.passenger = 4;
         car.printDataCar();
 
 
-        Car car2 = new Car();
-        car2.license = "JAK067";
-        car2.driver = "Paola Agudelo";
+        Car car2 = new Car("JAK067",new Account("Paola Agudelo", "PAO234"));
         car2.passenger = 3;
         car2.printDataCar();
         
